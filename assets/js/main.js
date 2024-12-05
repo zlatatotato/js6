448dc7b719caabc017d11e247cd4338fb765d4e4
zlatanchik666 <urazovazlata@gmail.com>
2024-12-05 23:50:15 +0300

    важные изменения

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// ------1------
+
+// let polz = document.body.children[0];
+// console.log(polz);
+
+
+// ------2------
+
+// let polz = document.body.children[1];
+// console.log(polz);
+
+
+// ------3------
+
+// let surname = prompt("Введите число", 'число');
+
+
+// for (let i = 0; i < surname; i++) {
+//     const div = document.createElement("div");
+//     div.textContent = 'Блок';
+//     div.style.border = "1px solid black";
+    
+//     document.body.append(div);
+// }
+
+
+
+
